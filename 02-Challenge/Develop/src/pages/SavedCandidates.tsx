b3b10eda90ba7c2624a98b3d9060d3b18da6677f
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2025-03-09 19:33:21 -0500

    working for sure

--- 02-Challenge/Develop/src/pages/SavedCandidates.tsx
+++ 02-Challenge/Develop/src/pages/SavedCandidates.tsx
@@ -15,6 +15,12 @@ const SavedCandidates = () => {
     getCandidates();
   }, [])
 
+  const removeCandidate = (index: number) => {
+    const updatedCandidates = candidates.filter((_, i) => i !== index);
+    setCandidates(updatedCandidates);
+    localStorage.setItem("candidates", JSON.stringify(updatedCandidates));
+  };
+
   return (
     <>
       <h1>Potential Candidates</h1>
@@ -22,20 +28,26 @@ const SavedCandidates = () => {
       <table className='table'>
         <thead>
           <tr>
+            <th>Avatar</th>
             <th>Login</th>
             <th>Bio</th>
             <th>Company</th>
             <th>Location</th>
+            <th>Remove Candidate</th>
           </tr>
         </thead>
 
         <tbody>
           {candidates.map((candidate, index) => (
             <tr key={index}>
+              <td>{candidate.avatar_url ? (<img src={candidate.avatar_url} alt={`${candidate.login}'s avatar`} width="50" />) : "https://placehold.co/400"}</td>
               <td>{candidate.login}</td>
               <td>{candidate?.bio ? candidate.bio : "No bio provided"}</td>
               <td>{candidate.company}</td>
               <td>{candidate.location}</td>
+              <td>
+                <button className='skipUser' onClick={() => removeCandidate(index)}>Remove</button>
+              </td>
             </tr>
           ))}
         </tbody>
